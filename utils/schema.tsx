75c02a61e7e6f88dea2d02368f28516e8ee9f277
Nawazwariya182 <wariyanawaz@gmail.com>
2024-12-30 23:18:59 +0530

    Fix: Corrected page export structure in translate page

--- utils/schema.tsx
+++ utils/schema.tsx
@@ -2,14 +2,14 @@ import { imageGen } from './schemas/imagegenschema';
 import { voiceGen } from './schemas/voicegenschema';
 import { entityLinking } from './schemas/entitylinkingschema';
 import { translate } from './schemas/translateschema';
-import { credit } from './schemas/credit';
+// import { credit } from './schemas/credit';
 
 export {
     imageGen,
     voiceGen,
     entityLinking,
     translate,
-    credit
+    // credit
 };
 
 // You can keep your existing aioutput schema here if needed
